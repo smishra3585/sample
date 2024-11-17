56c6137244a5e29439bfa086d0b1800d86ff5b5b
smishra3585 <sudhanshu@reingames.com>
2024-11-18 00:10:48 +0530

    Update App.java

--- src/main/java/ReinGames/ForGit2/App.java
+++ src/main/java/ReinGames/ForGit2/App.java
@@ -4,5 +4,7 @@ package ReinGames.ForGit2;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! 2");
+        System.out.println("Hello World! 3");
     }
 }
