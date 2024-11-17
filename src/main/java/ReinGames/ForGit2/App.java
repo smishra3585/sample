056e6ed492768231ea664b750d8b60439365255b
“smishra3585” <Sudhanshu@reingames.com>
2024-11-17 15:00:27 +0530

    first commit

---
+++ src/main/java/ReinGames/ForGit2/App.java
@@ -0,0 +1,8 @@
+package ReinGames.ForGit2;
+
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/ReinGames/ForGit2/Sample.java
@@ -0,0 +1,10 @@
+package ReinGames.ForGit2;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome code for github");
+		System.out.println("writing in github");
+	}
+
+}
