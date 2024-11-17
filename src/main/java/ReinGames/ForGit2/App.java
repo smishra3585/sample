2cab72e8208391101659404ba62b126d9b56f6f6
“smishra3585” <Sudhanshu@reingames.com>
2024-11-18 00:26:55 +0530

    hgfvcgj

--- src/main/java/ReinGames/ForGit2/App.java
+++ src/main/java/ReinGames/ForGit2/App.java
@@ -9,4 +9,50 @@ public class App {
         System.out.println("Hello World! 4");
         System.out.println("Hello World! 5");
     }
+   
+    public void a1() {
+    	
+    	
+    	System.out.println("Hello World! 2");
+        System.out.println("Hello World! 3");
+    	
+    	
+    	
+    }
+public void a2() {
+    	
+    	
+    	System.out.println("Hello World! 2");
+        System.out.println("Hello World! 3");
+    	
+    	
+    	
+    }
+public void a4() {
+	
+	
+	System.out.println("Hello World! 2");
+    System.out.println("Hello World! 3");
+	
+	
+	
+}
+public void a3() {
+	
+	
+	System.out.println("Hello World! 2");
+    System.out.println("Hello World! 3");
+	
+	
+	
+}
+public void a5() {
+	
+	
+	System.out.println("Hello World! 2");
+    System.out.println("Hello World! 3");
+	
+	
+	
+}
 }
